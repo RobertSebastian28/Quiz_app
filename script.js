17ba846601271c98326555245a66a2d5cd07c4b4
RobertSebastian28 <you@example.com>
2022-05-30 21:10:27 +0200

    third commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let cars = [
+    {
+        'car': 'Ford Mustang 500 GT',
+        'image': 'img/mustang.jpg',
+        'question': 'Welches Auto siehst du hier ?'
+    },
+    {
+        'car': 'Audi R8',
+        'image': 'img/audi.jpg',
+        'question': 'Welches Auto siehst du hier ?'
+    },
+    {
+        'car': 'VW Käfer ',
+        'image': 'img/vw.jpg',
+        'question': 'Welches Auto siehst du hier ?'
+    },
+    {
+        'car': 'Nissan GTR ',
+        'image': 'img/gtr.jpg',
+        'question': 'Welches Auto siehst du hier ?'
+    },
+];
